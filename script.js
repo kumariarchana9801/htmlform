fa00e8fc4e40bb10322a7ab0a7202d79d267bfbd
Archana <kumariarchana98013@gmail.com>
2023-09-19 20:49:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function formSubmit(event){
+						event.preventDefault()
+						console.log(event)
+						var userfirstname=document.getElementById('userfirstname').value
+						console.log(userfirstname)
+		
+						var userlastname=document.getElementById('userlastname').value
+						console.log(userlastname)
+		
+						var address=document.getElementById('address').value
+						console.log(address)
+		
+						var pincode=document.getElementById('pincode').value
+						console.log(pincode)
+		
+						var CountryName=document.getElementById('Country Name').value
+						console.log(Countryname)
+		
+						var StateName =document.getElementById('State Name').value
+						console.log(StateName)
+		
+					}
